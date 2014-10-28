c492e03b99db128b84e493d375162e7be7eb9d6d
QingpingMeng <missing1989@gmail.com>
2014-10-27 22:43:41 -0500

    ex3_finish

--- Machine_Learning/mlclass-ex3/predict.m
+++ Machine_Learning/mlclass-ex3/predict.m
@@ -21,10 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+%add 1s to X
+X=[ones(m,1) X];
 
-
-
-
+z2=X*Theta1';
+a2=sigmoid(z2);
+n=size(Theta2,2);
+a2=[ones(m,1) a2];
+z3=a2*Theta2';
+a3=sigmoid(z3);
+[v,p]=max(a3,[],2);
 
 
 
